7e9b9cc35b44d1d6aa02f237c321d993ae1e9c40
Fauzan Ishtiaq <ishtiaq.fauzan@gmail.com>
2026-05-25 23:07:15 +0200

    demo: drive LED + assert GPIO/register (showcases MISRA report) (#29)

--- src/main.c
+++ src/main.c
@@ -1,8 +1,18 @@
 #include <zephyr/kernel.h>
 #include <zephyr/sys/printk.h>
+#include <zephyr/drivers/gpio.h>
+
+static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(DT_ALIAS(led0), gpios);
 
 int main(void)
 {
     printk("Hello World! EmbedCI\n");
+
+    if (!gpio_is_ready_dt(&led)) {
+        printk("LED not ready\n");
+        return 0;
+    }
+    gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
+    printk("LED on\n");
     return 0;
 }
